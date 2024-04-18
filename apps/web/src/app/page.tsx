8be915349a7a7f09a8e41ec333de8cda30d32d13
toycrane <alwaysfun2183@gmail.com>
2024-04-18 15:11:05 +0900

    feat: Add components.json, Button component, and utils file

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page(): JSX.Element {
   return (
     <main>
-      <div className="font-medium">Hello</div>
-      <div className="font-semibold">Hello</div>
-      <div className="font-bold">Hello</div>
-      <div className="font-extrabold">Hello</div>
+      <div className="flex justify-center items-center flex-col gap-4">
+        <div className="font-medium">Hello</div>
+        <div className="font-semibold">Hello</div>
+        <div className="font-bold">Hello</div>
+        <div className="font-extrabold">Hello</div>
+        <Button size="sm">Hello</Button>
+        <Button>Hello</Button>
+        <Button size="lg">Hello</Button>
+      </div>
     </main>
   );
 }
