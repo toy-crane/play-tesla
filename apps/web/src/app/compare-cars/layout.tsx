22005517f1f4d3adc7c088450110550cc6a6cf70
toycrane <alwaysfun2183@gmail.com>
2024-04-18 15:44:48 +0900

    style: Update layout to use a div with specific class

--- apps/web/src/app/compare-cars/layout.tsx
+++ apps/web/src/app/compare-cars/layout.tsx
@@ -1,5 +1,7 @@
 function Layout(props: { children: React.ReactNode }) {
-  return <div>{props.children}</div>;
+  return (
+    <div className="bg-primary-foreground min-h-screen">{props.children}</div>
+  );
 }
 
 export default Layout;
