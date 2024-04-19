b48d602743bbe7313267677cf93980f57bb079c0
toycrane <alwaysfun2183@gmail.com>
2024-04-19 16:36:36 +0900

    feat: Add Supabase client creation and data retrieval

--- apps/web/src/app/compare-cars/page.tsx
+++ apps/web/src/app/compare-cars/page.tsx
@@ -1,6 +1,24 @@
+import { createClient } from "@/utils/supabase/server";
 import ShareButton from "./_components/share-button";
 
-function Page() {
+interface Props {
+  searchParams: { trimList: string };
+}
+
+async function Page({ searchParams }: Props) {
+  const trimList = decodeURIComponent(searchParams.trimList);
+  const [first, second] = trimList.split(",");
+  const supabase = createClient();
+  const { data, error } = await supabase.from("trims").select("*");
+  // .in("slug", [first, second]);
+  if (error) {
+    throw error;
+  }
+
+  console.log(data);
+  const primary = data[0];
+  const secondary = data[1];
+
   return (
     <div className="content-grid">
       <div className="flex justify-between items-center mt-4">
@@ -10,6 +28,7 @@ function Page() {
         </h1>
         <ShareButton />
       </div>
+      <div></div>
     </div>
   );
 }
