affb3eabb23e977186da9b526bfc27f22a3728fe
toycrane <alwaysfun2183@gmail.com>
2024-04-21 18:53:18 +0900

    feat: Add middleware function for logging request info

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+
+  console.log("middleware");
+
+  // x-pathname 헤더를 추가하여 요청 URL을 기록
+  requestHeaders.set("x-pathname", request.nextUrl.pathname);
+  requestHeaders.set("x-query-params", request.nextUrl.search);
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  return response;
+}
