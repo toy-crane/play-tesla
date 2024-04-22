3c48673dfe6aa8e2a6a87d06b91c7636acbd68e9
toycrane <alwaysfun2183@gmail.com>
2024-04-22 17:14:29 +0900

    style: Update layout class in compare-cars component

--- apps/web/src/app/(route)/compare-cars/layout.tsx
+++ apps/web/src/app/(route)/compare-cars/layout.tsx
@@ -1,6 +1,6 @@
 function Layout(props: { children: React.ReactNode }) {
   return (
-    <div className="bg-primary-foreground min-h-screen">
+    <div className="min-h-screen">
       <header className="content-grid bg-white sticky">
         <nav className="py-4 flex items-center">Logo</nav>
       </header>
