c060919355fff605451e3e5b5dc235801e9cbfcc
toycrane <alwaysfun2183@gmail.com>
2024-04-26 17:59:20 +0900

    refactor: Improve interior code handling in image.ts

--- apps/web/src/lib/image.ts
+++ apps/web/src/lib/image.ts
@@ -3,15 +3,22 @@ import type { Trim, Option } from "@/types/data";
 
 // 모든 트림에 대한 인테리어 코드를 가져오지 않음
 const defaultInterior = {
-  mx: "$IBC00",
-  m3: "$IPB3",
-  my: "$INPB0",
-  ms: "$IBE00",
+  modelx: "$IBC00",
+  model3: "$IPB3",
+  modely: "$INPB0",
+  models: "$IBE00",
 } as const;
 
+const getDefaultInteriorCode = (slug: string) => {
+  const [model, trim] = slug.split("-");
+  if (model === "model3" && trim === "performance") {
+    return "$IPB4";
+  }
+  return defaultInterior[model as keyof typeof defaultInterior];
+};
+
 const getCarImageUrl = (trim: Trim, option: Option, view: CarView) => {
-  const interiorCode =
-    defaultInterior[trim.models?.code as keyof typeof defaultInterior];
+  const interiorCode = getDefaultInteriorCode(trim.slug);
   return `https://dgfgljvxbegytbhujxbk.supabase.co/storage/v1/object/public/cars/${trim.models?.code}/${trim.code}/${option.color}-${option.wheel}-${interiorCode}-${view}`;
 };
 
