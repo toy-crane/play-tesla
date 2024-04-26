67131f215cb253173d6d9ce6f07f5aac81ca7822
toycrane <alwaysfun2183@gmail.com>
2024-04-26 18:51:36 +0900

    feat: Add horizontal and square logos for cars layout

--- apps/web/src/app/(route)/cars/[trim]/layout.tsx
+++ apps/web/src/app/(route)/cars/[trim]/layout.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Layout(props: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen">
       <header className="content-grid bg-white sticky">
-        <nav className="py-4 flex items-center">Logo</nav>
+        <nav className="pt-4 pb-2 flex items-center">
+          <Link href="/cars/model3-longrange">
+            <Image
+              alt="logo"
+              height="36"
+              src="/logo/horizontal_logo.svg"
+              width="144"
+            />
+          </Link>
+        </nav>
       </header>
       <div className="content-grid">{props.children}</div>
     </div>
