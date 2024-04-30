9a09620b3d7190b36a633326281a5bbbe1e601bd
toycrane <alwaysfun2183@gmail.com>
2024-04-30 17:16:55 +0900

    feat: Add site configuration and metadata for play-tesla

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,13 +1,85 @@
 import "./globals.css";
 
 import localFont from "next/font/local";
+import type { Metadata } from "next/types";
 import { Toaster } from "@/components/ui/toaster";
+import { siteConfig } from "@/config/site";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
   display: "swap",
   weight: "45 920",
 });
+
+export const metadata: Metadata = {
+  title: {
+    default: "테슬라 정보를 한 곳에서 | play-tesla",
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "테슬라 모델 Y",
+    "테슬라 모델 3",
+    "테슬라 모델 S",
+    "테슬라 모델 X",
+    "테슬라 모델 Y 가격",
+    "테슬라 모델 3 가격",
+    "테슬라 모델 S 가격",
+    "테슬라 모델 X 가격",
+    "테슬라 보조금",
+    "테슬라 할인",
+    "테슬라 가격",
+  ],
+  authors: [
+    {
+      name: siteConfig.author,
+      url: siteConfig.links.blog,
+    },
+  ],
+  creator: siteConfig.author,
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@alwaysfun2183",
+  },
+  icons: {
+    shortcut: [
+      { url: "/favicon-16x16.png", sizes: "16x16" },
+      { url: "/favicon-32x32.png", sizes: "32x32" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: {
+      rel: "mask-icon",
+      url: "/safari-pinned-tab.svg",
+      color: "#5bbad5",
+    },
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+  metadataBase: new URL("https://goparkgolf.app"),
+  alternates: {
+    canonical: "/",
+  },
+};
 
 export default function RootLayout({
   children,
---
+++ apps/web/src/config/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: "play-tesla",
+  description:
+    "플레이 테슬라와 함께 테슬라와 관련된 소식을 쉽고 빠르게 찾아보세요. 테슬라 전기차 보조금, 테슬라 차량 가격 변경 추이 등을 한눈에 확인 하실 수 있습니다. 여러분의 테슬라 여정을 더욱 편리하고, 빠르게 만들어 드리겠습니다.",
+  url: "https://www.playtesla.xyz",
+  ogImage: "https://www.playtesla.xyz/api/og",
+  author: "toy-crane",
+  links: {
+    twitter: "https://twitter.com/alwaysfun2183",
+    github: "https://github.com/toy-crane",
+    blog: "https://www.toycrane.xyz",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
