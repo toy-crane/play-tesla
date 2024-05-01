e36f0c3dd0513c963e3ffa8fffbd9f667b0ba5ce
toycrane <alwaysfun2183@gmail.com>
2024-05-01 21:14:47 +0900

    feat: Update layout and page components

--- apps/web/src/app/(route)/(home)/layout.tsx
+++ apps/web/src/app/(route)/(home)/layout.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+
 function Layout(props: { children: React.ReactNode }) {
-  return <div>{props.children}</div>;
+  return (
+    <div className="min-h-screen">
+      <header className="content-grid bg-white sticky">
+        <nav className="pt-4 pb-2 flex items-center justify-between">
+          <Link href="/cars/model3-longrange">
+            <Image
+              alt="logo"
+              height="36"
+              src="/logo/horizontal_logo.svg"
+              width="144"
+            />
+          </Link>
+        </nav>
+      </header>
+      <main className="content-grid">{props.children}</main>
+    </div>
+  );
 }
 
 export default Layout;
--- apps/web/src/app/(route)/(home)/page.tsx
+++ apps/web/src/app/(route)/(home)/page.tsx
@@ -1,3 +1,25 @@
-export default function Page(): JSX.Element {
-  return <main className="content-grid" />;
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div className="flex w-full flex-col items-center gap-28 py-[26vh] pt-[18vh]">
+      <div className="flex flex-col items-center space-y-3">
+        <h1 className="text-3xl font-semibold tracking-tighter md:text-6xl">
+          테슬라 가격에 관한 모든 것
+        </h1>
+        <p className="text-foreground md:text-2xl">
+          가장 빠르고, 쉽게 알아 보세요
+        </p>
+      </div>
+      <div className="flex flex-col gap-2 w-full max-w-[480px]">
+        <Button asChild size="lg" variant="outline">
+          <Link href="/cars/model3-longrange">지역별 테슬라 보조금 조회</Link>
+        </Button>
+        <Button size="lg">
+          <Link href="/prices/model3">테슬라 모델별 가격 변경 추이</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
