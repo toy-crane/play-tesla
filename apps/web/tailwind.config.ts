a783689c73c5e4f9bea76b23ce404d14728f2af8
toycrane <alwaysfun2183@gmail.com>
2024-05-01 20:39:21 +0900

    style: Remove unused "dark-tremor" theme

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -83,36 +83,6 @@ const config: Config = {
             inverted: colors.white,
           },
         },
-        // dark mode
-        "dark-tremor": {
-          brand: {
-            faint: "#0B1229",
-            muted: colors.blue[950],
-            subtle: colors.blue[800],
-            DEFAULT: colors.blue[500],
-            emphasis: colors.blue[400],
-            inverted: colors.blue[950],
-          },
-          background: {
-            muted: "#131A2B",
-            subtle: colors.gray[800],
-            DEFAULT: colors.gray[900],
-            emphasis: colors.gray[300],
-          },
-          border: {
-            DEFAULT: colors.gray[800],
-          },
-          ring: {
-            DEFAULT: colors.gray[800],
-          },
-          content: {
-            subtle: colors.gray[600],
-            DEFAULT: colors.gray[500],
-            emphasis: colors.gray[200],
-            strong: colors.gray[50],
-            inverted: colors.gray[950],
-          },
-        },
       },
       boxShadow: {
         // light
