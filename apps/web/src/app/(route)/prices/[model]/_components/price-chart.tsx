bade1aa26b7ab532a134adfde29c1ab8b7c160fb
toycrane <alwaysfun2183@gmail.com>
2024-05-01 19:57:04 +0900

    style: Update styling in PriceChart component

--- apps/web/src/app/(route)/prices/[model]/_components/price-chart.tsx
+++ apps/web/src/app/(route)/prices/[model]/_components/price-chart.tsx
@@ -18,11 +18,9 @@ export function PriceChart({
     const { payload, active, label } = props;
     if (!active || !payload) return null;
     return (
-      <div className="w-64 rounded-tremor-default border border-tremor-border bg-tremor-background p-2 text-tremor-default shadow-tremor-dropdown space-y-1">
-        <div className="border-tremor-border border-b dark:border-dark-tremor-border py-1">
-          <p className="font-medium text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis">
-            {label}
-          </p>
+      <div className="w-64 border p-2 space-y-1 rounded-md bg-card">
+        <div className="border-b py-1">
+          <span className="text-sm font-semibold">{label}</span>
         </div>
         <div className="py-1">
           {payload
@@ -31,13 +29,13 @@ export function PriceChart({
               <div className="flex flex-1 space-x-2.5 items-center" key={idx}>
                 <span
                   className={cn(
-                    "shrink-0 rounded-tremor-full border-2 h-3 w-3 border-tremor-background shadow-tremor-card dark:border-dark-tremor-background dark:shadow-dark-tremor-card",
+                    "shrink-0 rounded-full border-2 h-3 w-3 border-muted",
                     `bg-${category.color}`
                   )}
                 />
                 <div className="flex gap-2 flex-1 justify-between">
-                  <p className="text-tremor-content">{category.dataKey}</p>
-                  <p className="font-medium text-tremor-content-emphasis">
+                  <p className="text-muted-foreground">{category.dataKey}</p>
+                  <p className="font-medium">
                     {(Number(category.value) / 10000).toLocaleString()}만원
                   </p>
                 </div>
