db54e5bd5ee58e1834f62132bcb2f4b168412491
toycrane <alwaysfun2183@gmail.com>
2024-05-02 12:45:52 +0900

    feat: Add new dependencies and UI components

--- apps/web/src/app/(route)/prices/subscribe/page.tsx
+++ apps/web/src/app/(route)/prices/subscribe/page.tsx
@@ -1,29 +1,93 @@
+"use client";
+
+import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { toast } from "@/components/ui/use-toast";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+const FormSchema = z.object({
+  isAgree: z.boolean().default(false).optional(),
+});
+
 function Page() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      isAgree: true,
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
   return (
     <section className="w-full py-12 md:py-24 lg:py-32">
-      <div className="container mx-auto max-w-md px-4 md:px-6">
-        <div className="space-y-4 text-center">
+      <div className="mx-auto max-w-md px-4 md:px-6">
+        <div className="space-y-4 text-center mb-8">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">
-            Subscribe to our newsletter
+            테슬라 가격 변동 알림 받기
           </h2>
           <p className="text-gray-500 dark:text-gray-400">
-            Get the latest updates and news delivered to your inbox.
+            테슬라 차량의 가격 변동을 이메일 알림으로 가장 빠르게 받아보세요.
           </p>
         </div>
-        <form className="mt-8 space-y-4">
-          <div className="flex w-full max-w-sm items-center space-x-2">
-            <Input
-              className="flex-1"
-              placeholder="Enter your email"
-              required
-              type="email"
+        <Form {...form}>
+          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex w-full max-w-sm items-center space-x-2">
+              <Input
+                className="flex-1"
+                placeholder="이메일 입력하기"
+                required
+                type="email"
+              />
+              <Button type="submit">알림 받기</Button>
+            </div>
+            <FormField
+              control={form.control}
+              name="isAgree"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel
+                      className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      htmlFor="agreement"
+                    >
+                      <Link className="underline" href="#">
+                        개인 정보 수집 약관
+                      </Link>
+                      에 동의합니다.
+                    </FormLabel>
+                  </div>
+                </FormItem>
+              )}
             />
-            <Button type="submit">Subscribe</Button>
-          </div>
-        </form>
+          </form>
+        </Form>
       </div>
     </section>
   );
