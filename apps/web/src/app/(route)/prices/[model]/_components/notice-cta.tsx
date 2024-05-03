d4c92a04ece68429922e29f091b122d6e2b049a2
toycrane <alwaysfun2183@gmail.com>
2024-05-03 09:15:49 +0900

    feat: Add Link component for notice CTA and Popover for agreement text

--- apps/web/src/app/(route)/prices/[model]/_components/notice-cta.tsx
+++ apps/web/src/app/(route)/prices/[model]/_components/notice-cta.tsx
@@ -1,13 +1,16 @@
 import { AlarmClock } from "lucide-react";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 function NoticeCTA() {
   return (
     <div className="z-bottom-nav content-grid fixed bottom-4 w-full justify-center bg-gradient-to-t from-white from-80% to-transparent">
       <div className="md:content full flex justify-center pt-5">
-        <Button className="w-full text-sm" size="lg">
-          <AlarmClock className="w-4 h-4 mr-1" />
-          새로운 가격 변동 알림 받기
+        <Button asChild className="w-full text-sm" size="lg">
+          <Link href="/prices/subscribe">
+            <AlarmClock className="w-4 h-4 mr-1" />
+            새로운 가격 변동 알림 받기
+          </Link>
         </Button>
       </div>
     </div>
--- apps/web/src/app/(route)/prices/subscribe/page.tsx
+++ apps/web/src/app/(route)/prices/subscribe/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import Link from "next/link";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { toast } from "@/components/ui/use-toast";
@@ -13,14 +13,22 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Input } from "@/components/ui/input";
 
 const FormSchema = z.object({
   isAgree: z.boolean().default(false).optional(),
+  email: z.string().email("이메일 형식이 아닙니다."),
 });
 
 function Page() {
+  const [open, setOpen] = useState(false);
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -51,13 +59,24 @@ function Page() {
           </p>
         </div>
         <Form {...form}>
-          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
-            <div className="flex w-full max-w-sm items-center space-x-2">
-              <Input
-                className="flex-1"
-                placeholder="이메일 입력하기"
-                required
-                type="email"
+          <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex w-full max-w-sm space-x-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="flex flex-1 flex-col">
+                    <FormControl>
+                      <Input
+                        className="flex-auto"
+                        placeholder="이메일 입력하기"
+                        required
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
               <Button type="submit">알림 받기</Button>
             </div>
@@ -73,15 +92,32 @@ function Page() {
                     />
                   </FormControl>
                   <div className="space-y-1 leading-none">
-                    <FormLabel
-                      className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                      htmlFor="agreement"
-                    >
-                      <Link className="underline" href="#">
-                        개인 정보 수집 약관
-                      </Link>
-                      에 동의합니다.
-                    </FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormLabel
+                          className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                          htmlFor="agreement"
+                        >
+                          <Button
+                            className="underline px-0 py-0 h-fit hover:bg-white"
+                            onClick={() => {
+                              setOpen(true);
+                            }}
+                            type="button"
+                            variant="ghost"
+                          >
+                            개인 정보 수집 약관
+                          </Button>
+                          에 동의합니다.
+                        </FormLabel>
+                      </PopoverTrigger>
+                      <PopoverContent className="text-xs">
+                        play-tesla는 가격 알림을 발송을 위해 이메일 주소를
+                        수집하고자 합니다. 수집된 이메일은 가격 알림 이외의
+                        목적으로 사용되지 않으며, 서비스 종료 혹은 회원님의 요청
+                        시 즉시 삭제됩니다.
+                      </PopoverContent>
+                    </Popover>
                   </div>
                 </FormItem>
               )}
