52348e1e0b2782c3f7ba84e2f1934d27b4b4f135
toycrane <alwaysfun2183@gmail.com>
2024-07-30 20:48:59 +0900

    refactor: Update model identifier to modelCode

--- apps/web/src/app/api/get-car-price/route.ts
+++ apps/web/src/app/api/get-car-price/route.ts
@@ -4,8 +4,9 @@ import { createClient } from "@/utils/supabase/server";
 import { alertDiscord } from "@/lib/discord";
 
 interface TeslaResponse {
-  DSServices: {
-    "Lexicon.m3": {
+  DSServices: Record<
+    string,
+    {
       options: Record<
         string,
         {
@@ -15,27 +16,34 @@ interface TeslaResponse {
           }[];
         }
       >;
-    };
-  };
+    }
+  >;
 }
 
 interface Request {
-  model: string;
+  modelCode: keyof typeof modelSlug;
 }
 
-export async function POST(request: NextRequest) {
-  const { model } = (await request.json()) as Request;
+const modelSlug = {
+  m3: "model3",
+  my: "modely",
+  mx: "modelx",
+  ms: "models",
+};
 
-  if (!["model3", "modely", "modelx", "models"].includes(model)) {
-    return new Response("Invalid model", { status: 400 });
+export async function POST(request: NextRequest) {
+  const { modelCode } = (await request.json()) as Request;
+
+  if (!["m3", "my", "mx", "ms"].includes(modelCode)) {
+    throw new Error("Invalid model");
   }
 
-  const TESLA_URL = `https://www.tesla.com/ko_kr/${model}/design#overview`;
+  const TESLA_URL = `https://www.tesla.com/ko_kr/${modelCode}/design#overview`;
   const supabase = createClient({ type: "admin" });
   const { data, error } = await supabase
     .from("models")
     .select("trims(code, id, slug, trim_prices(price_set_at, price))")
-    .eq("slug", model);
+    .eq("slug", modelSlug[modelCode]);
 
   if (error) {
     throw new Error(error.message);
@@ -61,59 +69,44 @@ export async function POST(request: NextRequest) {
     })
   );
 
-  try {
-    const response = await fetch(TESLA_URL);
-    const html = await response.text();
-    // eslint-disable-next-line prefer-named-capture-group -- ignore
-    const pattern = /const dataJson = ({[\s\S]*?});/;
-    const match = pattern.exec(html);
+  const response = await fetch(TESLA_URL);
+  const html = await response.text();
+  // eslint-disable-next-line prefer-named-capture-group -- ignore
+  const pattern = /const dataJson = ({[\s\S]*?});/;
+  const match = pattern.exec(html);
 
-    if (!match?.[1]) throw new Error("Match not found");
+  if (!match?.[1]) throw new Error("Match not found");
 
-    try {
-      // JSON.parse를 사용하여 객체로 변환합니다
-      // eslint-disable-next-line @typescript-eslint/no-implied-eval, no-new-func -- ignore
-      const teslaResponse = new Function(
-        `return ${match[1]}`
-      )() as TeslaResponse;
+  // JSON.parse를 사용하여 객체로 변환합니다
+  // eslint-disable-next-line @typescript-eslint/no-implied-eval, no-new-func -- ignore
+  const teslaResponse = new Function(`return ${match[1]}`)() as TeslaResponse;
+  const modelDetail = teslaResponse.DSServices[`Lexicon.${modelCode}`];
+  if (!modelDetail) throw new Error("Model options not found");
+  const options = modelDetail.options;
 
-      const options = teslaResponse.DSServices["Lexicon.m3"].options;
-
-      for (const trim of trims) {
-        const pricingOptions = options[trim.code];
-        if (!pricingOptions) continue;
-        const priceOption = pricingOptions.pricing.find(
-          (p) => p.type === "base_plus_trim"
-        );
-        const latestPrice = trim.latest_price;
-        if (priceOption && latestPrice.price !== priceOption.value) {
-          // eslint-disable-next-line no-await-in-loop -- ignore
-          await supabase.from("trim_prices").insert({
-            price: priceOption.value,
-            price_set_at: new Date().toISOString(),
-            trim_id: trim.trim_id,
-          });
-          // eslint-disable-next-line no-await-in-loop -- ignore
-          await alertDiscord(
-            "https://discord.com/api/webhooks/1267804345362026537/YZBA5v6d5qen82sfFjEA6a3QbMGtF_Px9yIdzGFl3ehN1Z0H3Lemm0uvxY4aiPo4q1eC",
-            `차량 가격이 ${new Date().toLocaleDateString()}에 변경되었습니다. 차량 코드: ${trim.code}, 차량 이름: ${trim.slug} 기존 가격: ${latestPrice.price}, 새로운 가격: ${priceOption.value}`
-          );
-        }
-      }
-
-      return NextResponse.json({
-        result: "success",
+  for (const trim of trims) {
+    const pricingOptions = options[trim.code];
+    if (!pricingOptions) continue;
+    const priceOption = pricingOptions.pricing.find(
+      (p) => p.type === "base_plus_trim"
+    );
+    const latestPrice = trim.latest_price;
+    if (priceOption && latestPrice.price !== priceOption.value) {
+      // eslint-disable-next-line no-await-in-loop -- ignore
+      await supabase.from("trim_prices").insert({
+        price: priceOption.value,
+        price_set_at: new Date().toISOString(),
+        trim_id: trim.trim_id,
       });
-    } catch (e) {
-      return NextResponse.json(
-        { result: "error", message: "Failed to crawl Tesla page" },
-        { status: 500 }
+      // eslint-disable-next-line no-await-in-loop -- ignore
+      await alertDiscord(
+        "https://discord.com/api/webhooks/1267804345362026537/YZBA5v6d5qen82sfFjEA6a3QbMGtF_Px9yIdzGFl3ehN1Z0H3Lemm0uvxY4aiPo4q1eC",
+        `차량 가격이 ${new Date().toLocaleDateString()}에 변경되었습니다. 차량 코드: ${trim.code}, 차량 이름: ${trim.slug} 기존 가격: ${latestPrice.price}, 새로운 가격: ${priceOption.value}`
       );
     }
-  } catch (e) {
-    return NextResponse.json(
-      { result: "error", message: "Failed to crawl Tesla page" },
-      { status: 500 }
-    );
   }
+
+  return NextResponse.json({
+    result: "success",
+  });
 }
