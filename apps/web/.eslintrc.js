d0cb1e26076b6f57ea17dfd7311bb1226a128d22
toycrane <alwaysfun2183@gmail.com>
2024-07-31 12:24:35 +0900

    feat: Add eslint rule for no-console with exceptions

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ["@repo/eslint-config/next.js"],
+  rules: {
+    "no-console": ["error", { allow: ["warn", "error"] }],
+  },
   ignorePatterns: ["src/components/*", "src/types/generated.ts"],
 };
