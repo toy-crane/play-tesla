58db6d24b54f65200a1cf982c6ff2610ea5cdac0
toycrane <alwaysfun2183@gmail.com>
2024-07-31 12:46:51 +0900

    fix: Update fetch headers for Tesla API

--- apps/web/src/app/api/get-car-price/[modelCode]/route.ts
+++ apps/web/src/app/api/get-car-price/[modelCode]/route.ts
@@ -76,7 +76,19 @@ export async function GET(
     })
   );
 
-  const response = await fetch(TESLA_URL);
+  const response = await fetch(TESLA_URL, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.5",
+      "Accept-Encoding": "gzip, deflate, br",
+      Connection: "keep-alive",
+      "Upgrade-Insecure-Requests": "1",
+      "Cache-Control": "max-age=0",
+    },
+  });
   console.log("tesla html fetch success");
   const html = await response.text();
   // eslint-disable-next-line prefer-named-capture-group -- ignore
