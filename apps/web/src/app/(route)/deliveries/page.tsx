669c823cce0a13dd97072466a6a24bb4aaf0347e
toycrane <alwaysfun2183@gmail.com>
2024-08-02 11:16:03 +0900

    feat: Add new line chart component and recharts library

--- apps/web/src/app/(route)/deliveries/page.tsx
+++ apps/web/src/app/(route)/deliveries/page.tsx
@@ -1,6 +1,10 @@
+import Chart from "./_components/chart";
+
 function Page() {
   return (
-    <div className="w-full max-w-2xl mx-auto py-8 px-4 md:px-6">Hello</div>
+    <div className="w-full max-w-2xl mx-auto py-8 px-4 md:px-6">
+      <Chart />
+    </div>
   );
 }
 
