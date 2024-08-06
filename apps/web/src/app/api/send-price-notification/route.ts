720c812da15b9559b7e2486d2bbf971e8f3c9fb6
toycrane <alwaysfun2183@gmail.com>
2024-08-06 12:25:19 +0900

    feat(api): add price notification email functionality

---
+++ apps/web/src/app/api/send-price-notification/route.ts
@@ -0,0 +1,34 @@
+import type { NextRequest } from "next/server";
+import type { Tables } from "@/types/generated";
+
+const RESEND_API_KEY = process.env.RESEND_API_KEY!;
+
+interface Request {
+  type: "INSERT";
+  table: string;
+  schema: string;
+  record: Tables<"trim_prices">;
+  old_record: null;
+}
+
+export async function POST(request: NextRequest) {
+  const { record } = (await request.json()) as Request;
+  const res = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: "Play Tesla <notifications@playtesla.xyz>",
+      to: ["alwaysfun2183@gmail.com"],
+      subject: "차량 가격이 변동 되었습니다",
+      html: `<strong>${record.trim_id} 차량 가격이 변동 되었습니다.</strong>`,
+    }),
+  });
+
+  if (res.ok) {
+    const data = (await res.json()) as Record<string, unknown>;
+    return Response.json(data);
+  }
+}
