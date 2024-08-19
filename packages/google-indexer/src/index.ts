360c55b35a947d7209a2ab229fb35cf080a9bba3
toycrane <alwaysfun2183@gmail.com>
2024-08-19 12:00:08 +0900

    feat(google-indexer): add quota configuration to indexer

--- packages/google-indexer/src/index.ts
+++ packages/google-indexer/src/index.ts
@@ -4,6 +4,9 @@ import serviceAccount from "../service_account.json";
 index("playtesla.xyz", {
   client_email: serviceAccount.client_email,
   private_key: serviceAccount.private_key,
+  quota: {
+    rpmRetry: true,
+  },
 })
   .then(console.log)
   .catch(console.error);
